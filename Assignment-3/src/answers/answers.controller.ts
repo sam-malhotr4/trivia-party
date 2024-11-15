c4218fb0665e8382c9b6623aad4da62fb460e638
jskaran4323 <jskaran782742@gmail.com>
2024-11-15 11:33:55 -0500

    Added Roles to answer API

--- Assignment-3/src/answers/answers.controller.ts
+++ Assignment-3/src/answers/answers.controller.ts
@@ -10,7 +10,7 @@ import { Roles } from '../auth/roles.decorator';
 @UseGuards(JwtAuthGuard, RolesGuard) 
 export class AnswersController {
   constructor(private readonly answersService: AnswersService) { }
- 
+  @Roles("user","admin")
   @Post()
   async submitAnswer(@Body() answerDto: AnswerDto) {
     return this.answersService.checkAnswer(answerDto);
